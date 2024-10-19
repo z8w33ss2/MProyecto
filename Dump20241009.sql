e831b3425b09323316bde7a72e36347014b59d2f
z8w33ss2 <123525733+z8w33ss2@users.noreply.github.com>
2024-10-19 12:41:35 -0600

    Update Dump20241009.sql

--- Dump20241009.sql
+++ Dump20241009.sql
@@ -28,7 +28,7 @@ CREATE TABLE `trol` (
   `Consecutivo` int(11) NOT NULL AUTO_INCREMENT,
   `NombreRol` varchar(50) NOT NULL,
   PRIMARY KEY (`Consecutivo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `trol` (
 
 LOCK TABLES `trol` WRITE;
 /*!40000 ALTER TABLE `trol` DISABLE KEYS */;
+INSERT INTO `trol` VALUES (1,'Administrador'),(2,'Cliente');
 /*!40000 ALTER TABLE `trol` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,9 +57,11 @@ CREATE TABLE `tusuario` (
   `Activo` bit(1) NOT NULL,
   `ConsecutivoRol` int(11) DEFAULT NULL,
   PRIMARY KEY (`Consecutivo`),
+  UNIQUE KEY `CorreoElectronico_UNIQUE` (`CorreoElectronico`),
+  UNIQUE KEY `Identificacion_UNIQUE` (`Identificacion`),
   KEY `FK_ROL` (`ConsecutivoRol`),
   CONSTRAINT `FK_ROL` FOREIGN KEY (`ConsecutivoRol`) REFERENCES `trol` (`Consecutivo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,12 +70,53 @@ CREATE TABLE `tusuario` (
 
 LOCK TABLES `tusuario` WRITE;
 /*!40000 ALTER TABLE `tusuario` DISABLE KEYS */;
+INSERT INTO `tusuario` VALUES (1,'112430278','Carlos Solis A','csolis3027868@ufide.ac.cr','123456789',_binary '',2),(6,'123456789','Carlos Solis A','csolis30278689@ufide.ac.cr','1598753',_binary '',2);
 /*!40000 ALTER TABLE `tusuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
+-- Dumping events for database 'cursobd'
+--
+
+--
 -- Dumping routines for database 'cursobd'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `RegistrarUsuario` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarUsuario`(pIdentificacion varchar(20),pNombre varchar(255),pCorreoElectronico varchar(80),pContrasena varchar(10))
+BEGIN
+	INSERT INTO `cursobd`.`tusuario`
+	(
+	`Identificacion`,
+	`Nombre`,
+	`CorreoElectronico`,
+	`Contrasena`,
+	`Activo`,
+	`ConsecutivoRol`)
+	VALUES
+	(
+	pIdentificacion,
+	pNombre,
+	pCorreoElectronico,
+	pContrasena,
+	1,
+	2
+    );
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -83,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-09 21:42:36
+-- Dump completed on 2024-10-19 12:40:26
